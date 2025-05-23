84022366a34d696fa3191ce9d927091005ffe318
AbuRider <dinarysf276@gmail.com>
2025-05-23 17:58:01 +0700

    Update script.sh
    Signed-off-by: AbuRider <dinarysf276@gmail.com>

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/neonred226/android-rom-build -b derp-15.2 .repo/local_manifests
+git clone https://github.com/AbuRider/local_manifests -b 15.2 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -21,7 +21,7 @@ echo "Sync success"
 echo "============="
 
 # Export
-export BUILD_USERNAME=NEONXD
+export BUILD_USERNAME=kingtaksaka
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
@@ -30,4 +30,4 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 
 echo "======= Export Done ======"
-lunch lineage_topaz-bp1a-userdebug && mka derp
+lunch lineage_earth-bp1a-userdebug && mka derp
