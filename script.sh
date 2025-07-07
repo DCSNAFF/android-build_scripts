ed9086843e95c05a9af3bb0ae19fd0b470e625c7
AbuRider <dinarysf276@gmail.com>
2025-07-07 13:53:09 +0700

    Update script.sh
    Signed-off-by: AbuRider <dinarysf276@gmail.com>

--- script.sh
+++ script.sh
@@ -30,9 +30,6 @@ git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git
 
 git clone https://github.com/AbuRider/vendor_extra.git -b master vendor/lineage-priv/keys
 
-# fix eror
-# git clone --depth=1 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android-15.0.0_r32 prebuilts/clang/host/linux-x86
-
 # Export
 export BUILD_USERNAME=rmdw
 export BUILD_HOSTNAME=crave
